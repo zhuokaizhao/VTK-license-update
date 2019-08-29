88f792f36886ffba40e7f92bf293e70a0f3ad0bb
Zhuokai Zhao <zhuokai@uchicago.edu>
2019-08-29 16:34:59 -0400

    Identify holder.

--- modify_license.py
+++ modify_license.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import fileinput
 import sys
+import numpy as np
 
 def get_file_paths(root_dir, verbose):
     all_paths = []
@@ -18,25 +19,54 @@ def get_file_paths(root_dir, verbose):
 
     return all_paths + additional_paths
 
-def replace_file(file_path, new_content):
+def analyze_file(file_path, all_signs, all_possible_holders):
     start = 0
+    sign = None
+    result = []
     for cur_line in fileinput.input(file_path, inplace=1):
-        # check if we are in the range
-        if '/*====' in cur_line:
-            sys.stdout.write(cur_line)
-            start = 1
-        elif '====*/' in cur_line:
-            start = 0
-            sys.stdout.write(new_content[0])
-        elif '/*----' in cur_line:
-            sys.stdout.write(cur_line)
-            start = 1
-        elif '----*/' in cur_line:
-            start = 0
-            sys.stdout.write(new_content[1])
+        for i in range(len(all_signs)):
+            if all_signs[i] in cur_line:
+                start = 1
+                sign = all_signs[i]
+                sys.stdout.write(cur_line)
+            elif all_signs[i][::-1] in cur_line:
+                start = 0
+                sign = None
 
         if start:
+            # check which organization is responsible for the license
+            for i in range(len(all_possible_holders)):
+                if all_possible_holders[i] in cur_line.lower():
+                    result.append((sign, all_possible_holders[i]))
             sys.stdout.write('')
+        else:
+            sys.stdout.write(cur_line)
+
+    if not result:
+        result.append('No license information found')
+
+    # return the unique collection
+    return set(result)
+
+
+def modify_file(file_path, license_info):
+    if license_info[0] == 'No license information found':
+        print('Warning:', file_path, 'does not have a valid license info detected.')
+        return
+
+    start = 0
+    license_num = None
+    for cur_line in fileinput.input(file_path, inplace=1):
+        for i in range(len(license_info)):
+            if license_info[i][0] in cur_line:
+                start = 1
+                license_num = i
+                sys.stdout.write(cur_line)
+            elif license_info[i][0][::-1] in cur_line:
+                start = 0
+
+        if start:
+            sys.stdout.write(license_info[license_num][1] + ' license' + '\n')
         else:
             sys.stdout.write(cur_line)
 
@@ -49,12 +79,24 @@ def main(args):
     file_count = len(all_file_paths)
     print(file_count, " eligible files have been found")
 
-    # change all the files accordingly
-    new_content = ['New license information 1\n',
-                    'New license information 2\n']
+    # analyze the license information
+    all_licenses_results = []
+    all_signs = ['/*====', '/*----']
+    all_possible_holders = ['kitware', 'sandia']
     for cur_path in all_file_paths:
-        replace_file(cur_path, new_content)
+        license_result = analyze_file(cur_path, all_signs, all_possible_holders)
+        all_licenses_results.append(license_result)
 
+    if verbose:
+        print('All possible holders are:', all_possible_holders)
+        print('License information with order corresponding to above files are')
+        for result in all_licenses_results:
+            print(result)
+
+    # modify the files accordingly
+    for i in range(len(all_file_paths)):
+        license_info = list(all_licenses_results[i])
+        modify_file(all_file_paths[i], license_info)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
