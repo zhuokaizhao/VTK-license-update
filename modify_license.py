208c05aad78fdf7032f237d908ac6b2b67eaaa68
Zhuokai Zhao <zhuokai@uchicago.edu>
2019-09-04 16:56:56 -0400

    Tested with all VTK files.

--- modify_license.py
+++ modify_license.py
@@ -1,13 +1,21 @@
-import argparse
 import os
-import fileinput
 import sys
+import codecs
+import functools
+import argparse
+import fileinput
 import numpy as np
+from time import sleep
 
 def get_file_paths(root_dir, verbose):
     all_paths = []
     additional_paths = []
     for path in os.scandir(root_dir):
+        # skip third party folder
+        if path.name == 'ThirdParty':
+            print(path.name + ' has been skipped.')
+            continue
+
         full_path = root_dir + os.sep + path.name
         if path.is_file():
             if path.name.endswith('.cxx') or path.name.endswith('.h') or path.name.endswith('.cpp'):
@@ -19,28 +27,47 @@ def get_file_paths(root_dir, verbose):
 
     return all_paths + additional_paths
 
-def analyze_file(file_path, all_signs, all_possible_holders):
+def analyze_file(file_path, all_start_signs, all_stop_signs, all_possible_holders):
+    # check if file is empty
+    if os.stat(file_path).st_size == 0:
+        print('Warning: ' + file_path + ' is empty.')
+        result = []
+        result.append('No license information found')
+        return result
+
     start = 0
-    sign = None
+    just_change = 0
+    start_sign = None
+    stop_sign = None
     result = []
+
     for cur_line in fileinput.input(file_path, inplace=1):
-        for i in range(len(all_signs)):
-            if all_signs[i] in cur_line:
+        for i in range(len(all_start_signs)):
+            if (start == 0) and (all_start_signs[i] in cur_line):
                 start = 1
-                sign = all_signs[i]
-                sys.stdout.write(cur_line)
-            elif all_signs[i][::-1] in cur_line:
+                start_sign = cur_line
+                stop_sign = None
+                sys.stdout.write('')
+                break
+            elif (start == 1) and (all_stop_signs[i] in cur_line):
                 start = 0
-                sign = None
+                start_sign = None
+                stop_sign = cur_line
+                just_change = 1
+                break
 
         if start:
             # check which organization is responsible for the license
             for i in range(len(all_possible_holders)):
                 if all_possible_holders[i] in cur_line.lower():
-                    result.append((sign, all_possible_holders[i]))
+                    result.append((start_sign, all_possible_holders[i], stop_sign))
             sys.stdout.write('')
         else:
-            sys.stdout.write(cur_line)
+            if just_change:
+                sys.stdout.write('')
+                just_change = 0
+            else:
+                sys.stdout.write(cur_line)
 
     if not result:
         result.append('No license information found')
@@ -54,37 +81,46 @@ def modify_file(file_path, license_info):
         print('Warning:', file_path, 'does not have a valid license info detected.')
         return
 
-    start = 0
+    # start = 0
+    modified = 0
     license_num = None
     for cur_line in fileinput.input(file_path, inplace=1):
-        for i in range(len(license_info)):
-            if license_info[i][0] in cur_line:
-                start = 1
-                license_num = i
-                sys.stdout.write(cur_line)
-            elif license_info[i][0][::-1] in cur_line:
-                start = 0
+        if not modified:
+            for i in range(len(license_info)):
+                sys.stdout.write(license_info[i][0])
+                if '//====' in license_info[i][0]:
+                    sys.stdout.write('// Plese check ' + license_info[i][1] + '_license.txt for license information.\n')
+                    sys.stdout.write(license_info[i][0])
+                else:
+                    sys.stdout.write('Plese check ' + license_info[i][1] + '_license.txt for license information.')
+                    sys.stdout.write(license_info[i][0][::-1] + '\n')
 
-        if start:
-            sys.stdout.write(license_info[license_num][1] + ' license' + '\n')
-        else:
-            sys.stdout.write(cur_line)
+            modified = 1
+
+        sys.stdout.write(cur_line)
 
 def main(args):
     verbose = int(args.verbose)
 
     # get all the eligible file paths
+    print('Finding all eligible files')
     file_count = 0
     all_file_paths = get_file_paths(args.path, verbose)
     file_count = len(all_file_paths)
-    print(file_count, " eligible files have been found")
+    print(file_count, "eligible files have been found")
 
     # analyze the license information
     all_licenses_results = []
-    all_signs = ['/*====', '/*----']
+    all_start_signs = ['/*====', '/*----', '//====', '/*\n']
+    all_stop_signs = ['====*/', '----*/', '//====', '*/']
+    # start and stop signs need to be the same sized
+    if (len(all_start_signs) != len(all_stop_signs)):
+        print('Error: start and stop signs need to be the same sized')
+
     all_possible_holders = ['kitware', 'sandia']
     for cur_path in all_file_paths:
-        license_result = analyze_file(cur_path, all_signs, all_possible_holders)
+        # print(cur_path)
+        license_result = analyze_file(cur_path, all_start_signs, all_stop_signs, all_possible_holders)
         all_licenses_results.append(license_result)
 
     if verbose:
@@ -94,9 +130,17 @@ def main(args):
             print(result)
 
     # modify the files accordingly
+    print('Modifying all the eligible files')
     for i in range(len(all_file_paths)):
+        percent = float((i+1)/len(all_file_paths))
+        sys.stdout.write('\r')
         license_info = list(all_licenses_results[i])
         modify_file(all_file_paths[i], license_info)
+        sys.stdout.write('[%-40s] %d%%' % ('='*int(percent*40), 100*percent))
+        sys.stdout.flush()
+        sleep(0.05)
+
+    sys.stdout.write('\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
